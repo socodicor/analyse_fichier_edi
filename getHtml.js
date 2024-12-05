5d3915a380878529509824478fa32b7ff8a1f6d6
Nicolas Ramel <nicolas.ramel@gmail.com>
2024-12-05 11:31:35 +0100

    affichage des n° de RANG

--- getHtml.js
+++ getHtml.js
@@ -8,7 +8,9 @@ function writeHTML(lines, config) {
   const descriptionEntete = getData.getDescriptionEntete(configFournisseur);
   const descriptionLigne = getData.getDescriptionLigne(configFournisseur);
 
-  let getHtml = "<html><head><title>Audit EDI</title></head><body>";
+  let getHtml = `<html><head><title>Config EDI : ${config.name}</title>`;
+  getHtml += `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">`;
+  getHtml += `</head><body><div class="container">`;
   getHtml += `<h1>Audit EDI : ${config.name}</h1>`;
 
   for (const line of lines) {
@@ -20,7 +22,7 @@ function writeHTML(lines, config) {
     }
   }
 
-  getHtml += "</body></html>";
+  getHtml += "</div></body></html>";
 
   // Écriture du fichier HTML
   fs.writeFileSync(path.join(__dirname, config.outputFile), getHtml, "utf-8");
@@ -29,13 +31,15 @@ function writeHTML(lines, config) {
 }
 
 function writeConfig(config, descriptionEntete, descriptionLigne) {
-  let getHtml = `<html><head><title>Config EDI : ${config.name}</title></head><body>`;
+  let getHtml = `<html><head><title>Config EDI : ${config.name}</title>`;
+  getHtml += `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">`;
+  getHtml += `</head><body><div class="container">`;
 
   getHtml += `<h1>Config EDI : ${config.name}</h1>`;
   getHtml += getTableauDescriptifConfig("ENTETE", descriptionEntete);
   getHtml += getTableauDescriptifConfig("LIGNE", descriptionLigne);
 
-  getHtml += "</body></html>";
+  getHtml += "</div></body></html>";
 
   // Écriture du fichier HTML
   fs.writeFileSync(
@@ -47,8 +51,9 @@ function writeConfig(config, descriptionEntete, descriptionLigne) {
 
 function getTableauDescriptifConfig(libelle, colonnes) {
   let html = `<h2>${libelle}</h2>`;
-  html += '<table border="1" style="width: 100%; border-collapse: collapse;">';
+  html += '<table class="table table-striped">';
   html += "<thead><tr>";
+  html += "<th>Rang</th>";
   html += "<th>Nom</th>";
   html += "<th>Type</th>";
   html += "<th>Longueur</th>";
@@ -56,8 +61,12 @@ function getTableauDescriptifConfig(libelle, colonnes) {
   html += "<th>Fin</th>";
   html += "<th>Commentaire</th>";
   html += "</tr></thead><tbody>";
+
+  let nColonne = 0;
   for (const colonne of colonnes) {
+    nColonne++;
     html += `<tr>`;
+    html += `<td>${nColonne}</td>`;
     html += `<td>${colonne.name}</td>`;
     html += `<td>${colonne.type}</td>`;
     html += `<td>${colonne.length}</td>`;
@@ -73,7 +82,7 @@ function getTableauDescriptifConfig(libelle, colonnes) {
 
 function getHtmlEntete(line, descriptionEntete) {
   let html = `<h2>ENTETE</h2>`;
-  html += '<table border="1" style="width: 100%; border-collapse: collapse;">';
+  html += '<table class="table table-striped">';
   html += "<thead><tr><th>Colonne</th><th>Valeur</th></tr></thead><tbody>";
   for (const colonne of descriptionEntete) {
     const content = line.substring(colonne.start - 1, colonne.end);
@@ -85,7 +94,7 @@ function getHtmlEntete(line, descriptionEntete) {
 
 function getHtmlLigne(line, descriptionLigne) {
   let html = `<h2>LIGNE</h2>`;
-  html += '<table border="1" style="width: 100%; border-collapse: collapse;">';
+  html += '<table class="table table-striped">';
   html += "<thead><tr><th>Colonne</th><th>Valeur</th></tr></thead><tbody>";
   for (const colonne of descriptionLigne) {
     const content = line.substring(colonne.start - 1, colonne.end);
